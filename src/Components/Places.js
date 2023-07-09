0cda092c5285a9bcca972eee9e3052a15f3f68ef
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-07-09 22:08:13 +0530

    updated places.js

--- src/Components/Places.js
+++ src/Components/Places.js
@@ -7,15 +7,16 @@ import PlacesRender from './PlacesRender'
 const Places = () => {
   const { continent, continentId, country, countryId } = useParams()
   const { continents } = useContext(contextData)
-  const placesArr = continents.find((item) => item.id === Number(continentId))
-  const { countries } = placesArr
-  console.log(countries)
+  const continentObj = continents[Number(continentId - 1)]
+  const placeArr = continentObj.countries.find((item) => item.id === Number(countryId))
+  const { destinations } = placeArr
+  
   return (
     <div>
       <h1>{continent}</h1>
       <h3>{country}</h3>
       <div className='main-container'>
-        {/* {destinations?.map((item, id) => <PlacesRender item={item} key={id} continent={continent} continentId={continentId} />)} */}
+        {destinations?.map((item, id) => <PlacesRender item={item} key={id} continent={continent} continentId={continentId} />)}
 
       </div>
     </div>
